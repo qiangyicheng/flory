37d3992a80d79a53891f573204ff90067b9adbe7
Yicheng Qiang <yicheng.qiang@ds.mpg.de>
2024-08-13 12:00:27 +0200

    version from git tag

--- flory/__init__.py
+++ flory/__init__.py
@@ -2,4 +2,17 @@
 Package :mod:`flory` provides methods and classes for phase separation in multicomponent mixtures based on Flory-Huggins theory.
 """
 
+try:
+    # try reading version of the automatically generated module
+    from ._version import __version__
+except ImportError:
+    from importlib.metadata import version, PackageNotFoundError
+
+    try:
+        __version__ = version("flory")
+    except PackageNotFoundError:
+        # package is not installed
+        __version__ = "unknown"
+    del PackageNotFoundError, version
+
 from .mcmp import CoexistingPhasesFinder, find_coexisting_phases
