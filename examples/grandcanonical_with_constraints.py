ab4e296be32f5ceb2a0e25b1a7b7d0ff92a132bb
Yicheng Qiang <yicheng.qiang@ds.mpg.de>
2024-08-29 15:46:01 +0200

    docs: add example for grand canonical ensemble

---
+++ examples/grandcanonical_with_constraints.py
@@ -0,0 +1,28 @@
+import numpy as np
+import flory
+
+num_comp = 3
+chis = (1 - np.identity(num_comp)) * 5
+
+mus = [-1, 0.0, 0.0]
+
+Cs = [[1, 1, 0]]
+Ts = [0.4]
+
+fh = flory.FloryHuggins(num_comp, chis)
+ensemble = flory.GrandCanonicalEnsemble.from_chemical_potential(num_comp, mus)
+constraint = flory.LinearGlobalConstraint(num_comp, Cs, Ts)
+
+finder = flory.CoexistingPhasesFinder(
+    fh.interaction,
+    fh.entropy,
+    ensemble,
+    constraint,
+    random_std=1.0,  # use less aggressive randomness to avoid rapid dying of compartments
+    progress=True,
+)
+volumes, phis = finder.run()
+
+with open(__file__ + ".out", "w") as f:
+    print("Volumes:", volumes, file=f)
+    print("Compositions:", phis, file=f)
