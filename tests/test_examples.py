c5672598979a9ceb88ca6e842de33aa816472c37
David Zwicker <david.zwicker@ds.mpg.de>
2024-08-30 17:14:51 +0200

    Run examples in temporary directory in tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,6 +5,7 @@
 import os
 import subprocess as sp
 import sys
+import shutil
 from pathlib import Path
 
 import pytest
@@ -14,16 +15,20 @@ EXAMPLES = (PACKAGE_PATH / "examples").glob("*.py")
 
 
 @pytest.mark.parametrize("path", EXAMPLES)
-def test_example_scripts(path):
+def test_example_scripts(path, tmp_path):
     """Runs an example script given by path."""
     # check whether this test needs to be run
     if path.name.startswith("_"):
         pytest.skip("skip examples starting with an underscore")
 
+    # copy the example to a temporary path
+    script = tmp_path / path.name
+    shutil.copy(path, script)
+
     # run the actual test in a separate python process
     env = os.environ.copy()
     env["PYTHONPATH"] = str(PACKAGE_PATH) + ":" + env.get("PYTHONPATH", "")
-    proc = sp.Popen([sys.executable, path], env=env, stdout=sp.PIPE, stderr=sp.PIPE)
+    proc = sp.Popen([sys.executable, script], env=env, stdout=sp.PIPE, stderr=sp.PIPE)
     try:
         outs, errs = proc.communicate(timeout=30)
     except sp.TimeoutExpired:
